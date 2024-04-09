44af01e1be51dccd89c34c4f74671c9c0c0c8cd9
daniel ung <daniel.ung@sjsu.edu>
2024-04-09 13:37:06 -0700

    get10Q filing method added

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 Financial-GPT
+by JaguarDB
+
+HOW TO USE :
+
+-Get SEC-API key off sec-api and put it into .env variable :
+https://sec-api.io/
+-Download requirements.txt in a conda env or whatever you choose 
+
+
+Run:
+python3 get10Q.py (TICKER_SYMBOL) to retreive 10-Q of that ticker symbol 
+
+
+to understand more of SEC_API, look at methods of the api/sec_api.py 
+
+Returned_data is the returned values of those methods, such as query and extract. 
+
+-the main python3 get10Q.py uses the query and download to check if a ticker exists and then downloads the 10-Q filings of that ticker. 
+
+
+
+
+
--- tests/unitTesting.py
+++ tests/unitTesting.py
@@ -8,26 +8,56 @@ from api.sec_api import SEC_API
 class TestSEC_API(unittest.TestCase):
     def setUp(self):
         self.api = SEC_API()
+    
 
+    """
+    Test the download method of the SEC_API class.
+
+    The download method should download the filing document in PDF Format.
+    -This method should return something called filing.pdf, the link currently links to Amazon 10-Q filing.
+    """
     def test_download(self):
         url = 'https://www.sec.gov/Archives/edgar/data/1018724/000144530513002495/amzn-20130930x10q.htm'
         result = self.api.download(url)
         # Add assertions here based on what you expect the result to be
         # For example, if download returns the content of the file, you might check if result is not empty
+        
+        print("RESULT: " , result)
         self.assertIsNotNone(result)
 
+
+
+    """
+    Test the extract method of the SEC_API class.
+
+    The extract method should extract the text of a specific item from the filing document.
+    -This method should return the text of a specific item from the filing document.
+
+    -look into extracted.txt in returnedData for the expected output
+    
+    """
     def test_extract(self):
         # Add your test code here
         link = "https://www.sec.gov/Archives/edgar/data/1018724/000144530513002495/amzn-20130930x10q.htm"
 
         # Use the extract method with the given link
         data = self.api.extract(link, "part1item1")
+        print("data: " , data)
 
-        print(data)
+
+
+    """
+    Test the query method of the SEC_API class.
+
+    The query method should query for a given ticker symbol given a ticker
+
+    -Look into query.txt for expected output 
+    
+    """
 
     def test_query (self):
         # Retrieve the 10-Q filings for a given ticker symbol
         ticker_symbol = 'AMZN'
 
         filings = self.api.query(ticker_symbol)
-        print(filings)     
+        print("filings", filings)     
